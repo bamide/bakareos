906369ff2fb12d026a80c5b8889ffda79261df26
bamide <bakremayowa2019@gmail.com>
2023-12-25 13:55:37 -0800

    update my shellos

--- main.c
+++ main.c
@@ -1,76 +1,93 @@
 #include "main.h"
 
-int main(int ac, char **argv)
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
 {
-    char *prompt = "(Mshell) $ ";
-    char *lineptr = NULL, *lineptr_copy = NULL;
-    size_t n = 0;
-    ssize_t nchars_read;
-    const char *delim = " \n";
-    int num_tokens = 0;
-    char *token;
-    int i;
-  
-    
-    /* Declare void variables */
-    (void)ac;
+ char **token_array;
+ char *line = NULL;
+ char *token;
+ int i, status;
+ pid_t child_pid;
+ ssize_t nread;
+ size_t len = 0;
 
-    /* Create a loop for the shell's prompt */
-    while (1)
+ (void)argc;
+ (void)argv;
+
+ while (1)
+ {
+  write(STDOUT_FILENO, "#cisfun$ ", 9);
+  nread = getline(&line, &len, stdin);
+  if (nread == -1)
+  {
+   perror("Error: Failed to read input");
+   exit(EXIT_FAILURE);
+  }
+
+  if (nread == 1)
+   continue;
+
+  token_array = malloc(sizeof(char *) * MAX_TOKENS);
+  if (token_array == NULL)
+  {
+   perror("Error: Failed to allocate memory");
+   exit(EXIT_FAILURE);
+  }
+
+  token = strtok(line, DELIM);
+  i = 0;
+  while (token && i < MAX_TOKENS - 1)
+  {
+   token_array[i] = token;
+   token = strtok(NULL, DELIM);
+   i++;
+  }
+  token_array[i] = NULL;
+
+  if (strcmp(token_array[0], "cd") == 0)
+  {
+   if (token_array[1] == NULL)
+   {
+    fprintf(stderr, "Error: Missing argument for cd\n");
+   }
+   else if (chdir(token_array[1]) != 0)
+   {
+    perror("Error: Failed to change directory");
+   }
+  }
+  else if (strcmp(token_array[0], "exit") == 0)
+  {
+   break;
+  }
+  else
+  {
+   child_pid = fork();
+   if (child_pid == -1)
+   {
+    perror("Error: Failed to create process");
+    exit(EXIT_FAILURE);
+   }
+   else if (child_pid == 0)
+   {
+    if (execvp(token_array[0], token_array) == -1)
     {
-        printf("%s", prompt);
-        nchars_read = getline(&lineptr, &n, stdin);
-        
-        /* Check if the getline function failed or reached EOF or the user used CTRL + D */
-        if (nchars_read == -1)
-        {
-            printf("Exiting shell....\n");
-            return (-1);
-        }
-
-        /* Allocate space for a copy of the lineptr */
-        lineptr_copy = malloc(sizeof(char) * nchars_read);
-        if (lineptr_copy == NULL)
-        {
-            perror("tsh: memory allocation error");
-            return (-1);
-        }
-        
-        /* Copy lineptr to lineptr_copy */
-        strcpy(lineptr_copy, lineptr);
-        /********** Split the string (lineptr) into an array of words ********/
-        
-        /* Calculate the total number of tokens */
-        token = strtok(lineptr, delim);
-
-        while (token != NULL)
-        {
-            num_tokens++;
-            token = strtok(NULL, delim);
-        }
-        num_tokens++;
-
-        /* Allocate space to hold the array of strings */
-        argv = malloc(sizeof(char *) * num_tokens);
-
-        /* Store each token in the argv array */
-        token = strtok(lineptr_copy, delim);
-        
-        for (i = 0; token != NULL; i++)
-        {
-            argv[i] = malloc(sizeof(char) * strlen(token));
-            strcpy(argv[i], token);
-            token = strtok(NULL, delim);
-        }
-        argv[i] = NULL;
-
-        /* Execute the command */
-        execmd(argv);
+     perror("Error: Failed to execute command");
+     exit(EXIT_FAILURE);
     }
+   }
+   else
+   {
+    wait(&status);
+   }
+  }
 
-    /* Free up allocated memory */
-    free(lineptr_copy);
-    free(lineptr);
+  free(token_array);
+ }
 
-    return (0);
+ free(line);
+ return (0);
 }
+
--- main.h
+++ main.h
@@ -1,9 +1,15 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#define MAX_TOKENS 64
+#define DELIM " \t\r\n\a"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <sys/stat.h>
+#include <sys/wait.h>
 
-void execmd(char **argv);
-char *get_location(char *command);
+#endif /* SHELL_H */
+
